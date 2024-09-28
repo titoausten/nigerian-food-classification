d102f0568288fd7234c05d6ce429f35fcfb75856
Oyefusi Samuel <105239214+Oyefusi-Samuel@users.noreply.github.com>
2024-09-28 23:18:59 +0100

    Comment-Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from src.ngaFoodClassifier import logger
-from src.ngaFoodClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ngaFoodClassifier import logger  # Importing the logger for logging events
+from src.ngaFoodClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline  # Importing the data ingestion pipeline
 
-
+# Define the name of the current stage for logging purposes
 STAGE_NAME = "Data Ingestion stage"
+
 try:
+    # Log the start of the data ingestion stage
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    
+    # Create an instance of the DataIngestionTrainingPipeline
     ingestion = DataIngestionTrainingPipeline()
+    
+    # Execute the main method of the ingestion pipeline
     ingestion.main()
+    
+    # Log the completion of the data ingestion stage
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
+    # Log the exception if any error occurs during the process
     logger.exception(e)
-    raise e
+    raise e  # Re-raise the exception to propagate it further
