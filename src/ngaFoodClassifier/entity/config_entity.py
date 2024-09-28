9fc23a95650ef1a45d218bb67cc86639fb2e548f
Oyefusi Samuel <105239214+Oyefusi-Samuel@users.noreply.github.com>
2024-09-28 23:09:09 +0100

    Update config_entity.py-commented

--- src/ngaFoodClassifier/entity/config_entity.py
+++ src/ngaFoodClassifier/entity/config_entity.py
@@ -4,7 +4,22 @@ from pathlib import Path
 
 @dataclass(frozen=True)
 class DataIngestionConfig:
-    root_dir: Path
-    source_URL: str
-    local_data_file: Path
-    unzip_dir: Path
+    """
+    Configuration class for data ingestion settings.
+    
+    Attributes:
+    -----------
+    root_dir : Path
+        The root directory where data files are stored.
+    source_URL : str
+        The URL from which to download the data file.
+    local_data_file : Path
+        The local path for saving the downloaded file.
+    unzip_dir : Path
+        The directory where the contents of the zip file will be extracted.
+    """
+    
+    root_dir: Path              # Directory to store all data-related files
+    source_URL: str             # URL to download the data file from
+    local_data_file: Path       # Path where the downloaded file will be saved
+    unzip_dir: Path             # Path where the zip file's contents will be extracted
