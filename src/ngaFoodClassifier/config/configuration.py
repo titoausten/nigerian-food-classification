1459254184cb4cc0e7120cb3624a77931bd8a60b
ChidiebereMicah <111301657+ChidiebereMicah@users.noreply.github.com>
2024-10-01 12:31:16 +0100

    Added docstrings to configuration.py

--- src/ngaFoodClassifier/config/configuration.py
+++ src/ngaFoodClassifier/config/configuration.py
@@ -2,22 +2,45 @@ from src.ngaFoodClassifier.constants import *
 import os
 from pathlib import Path
 from src.ngaFoodClassifier.utils.common import read_yaml, create_directories
-from src.ngaFoodClassifier.entity.config_entity import (DataIngestionConfig)
+from src.ngaFoodClassifier.entity.config_entity import DataIngestionConfig
 
 
 class ConfigurationManager:
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH):
-        #params_filepath = PARAMS_FILE_PATH):
+    """
+    Handles the reading of configuration files and the setup of necessary directories for data ingestion.
 
+    Attributes
+    ----------
+    config : dict
+        The configuration settings read from the YAML file.
+    """
+
+    def __init__(self, config_filepath: str = CONFIG_FILE_PATH):
+        """
+        Initializes the ConfigurationManager with the specified configuration file path.
+        
+        It reads the YAML configuration file and creates necessary directories as specified in the configuration.
+
+        Parameters
+        ----------
+        config_filepath : str, optional
+            The file path to the configuration YAML file, by default `CONFIG_FILE_PATH`.
+        """
         self.config = read_yaml(config_filepath)
-        #self.params = read_yaml(params_filepath)
-
         create_directories([self.config.artifacts_root])
 
     
     def get_data_ingestion_config(self) -> DataIngestionConfig:
+        """
+        Prepares and returns the data ingestion configuration based on the YAML configuration.
+
+        This method creates the root directory for data ingestion and initializes a `DataIngestionConfig` object.
+
+        Returns
+        -------
+        DataIngestionConfig
+            A configuration object that contains the necessary paths and URLs for data ingestion.
+        """
         config = self.config.data_ingestion
 
         create_directories([config.root_dir])
