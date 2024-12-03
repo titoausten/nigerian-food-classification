6f6804701633ccd943fea9207f83130803e98b11
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2024-12-03 16:05:22 +0000

    Update data_transformation.py

--- src/ngaFoodClassifier/components/data_transformation.py
+++ src/ngaFoodClassifier/components/data_transformation.py
@@ -1,9 +1,10 @@
 from src.ngaFoodClassifier.constants import *
 from src.ngaFoodClassifier import logger
-#from src.ngaFoodClassifier.utils.common import get_size
+from src.ngaFoodClassifier.utils.common import get_size, create_directories
 from src.ngaFoodClassifier.entity.config_entity import DataTransformationConfig
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+import torch
 
 
 class DataTransformation:
@@ -25,8 +26,23 @@ class DataTransformation:
         train_data = datasets.ImageFolder(self.config.train_data_dir, transform=data_transform)
         test_data = datasets.ImageFolder(self.config.test_data_dir, transform=transforms.ToTensor())
 
+        # Save Tensors
+        self.save_tensor(train_data, ['train_tensors'])
+        self.save_tensor(test_data, ['test_tensors'])
         return train_data, test_data
 
+
+    def save_tensor(self, data, folder: list):
+        folder_path = self.config.root_dir
+        create_directories(folder_path)
+
+        for i, (image, label) in enumerate(data):
+            image_path = os.path.join(folder_path, folder[0], f'{i}.pt')
+            torch.save((image, label), image_path)
+
+            if i % 100 == 0:
+                logger.info(f'Saved {i} tensors to {image_path} folder')
+                
 
     def create_dataloaders(self):
         # Get class names
