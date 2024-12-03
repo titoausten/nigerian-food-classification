2dfeb1ceeffedfefc5cfd0967371010702579781
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2024-12-03 18:56:08 +0000

    Update __init__.py

--- src/ngaFoodClassifier/constants/__init__.py
+++ src/ngaFoodClassifier/constants/__init__.py
@@ -4,8 +4,8 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 NUM_WORKERS = os.cpu_count()
-MEAN = (0.5, 0.5, 0.5)
-STD = (0.5, 0.5, 0.5)
+MEAN = (0.6324, 0.5163, 0.3667)
+STD = (0.2613, 0.2721, 0.3229)
 """
 Module-level constants defining file paths for configuration and parameter files.
 
