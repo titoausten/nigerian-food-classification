8d0e870aa71a6cb0e30bd84c415093d91e7b5381
Tito Osadebey <94079649+titoausten@users.noreply.github.com>
2025-01-06 17:23:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from src.ngaFoodClassifier import logger  # Importing the logger for logging events
-from src.ngaFoodClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline  # Importing the data ingestion pipeline
+from src.ngaFoodClassifier import logger
+from src.ngaFoodClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.ngaFoodClassifier.pipeline.stage_02_data_transformation import DataTransformationPipeline
 
-# Define the name of the current stage for logging purposes
+
 STAGE_NAME = "Data Ingestion stage"
-
 try:
-    # Log the start of the data ingestion stage
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
-    
-    # Create an instance of the DataIngestionTrainingPipeline
-    ingestion = DataIngestionTrainingPipeline()
-    
-    # Execute the main method of the ingestion pipeline
+    ingestion = DataIngestionPipeline()
     ingestion.main()
-    
-    # Log the completion of the data ingestion stage
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    # Log the exception if any error occurs during the process
     logger.exception(e)
-    raise e  # Re-raise the exception to propagate it further
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    transformation = DataTransformationPipeline()
+    transformation.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
